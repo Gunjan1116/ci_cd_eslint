25b9ac3ce37672416a9af378575fc528fd08717a
Gunjan1116 <kumargunjan1116@gmail.com>
2023-02-19 00:17:55 +0530

    added change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app=require("express")();
 
 app.get("/",(req,res)=>{
-    req.send("Hello!!")
+    res.send("Hello!!")
 })
 
-app.listen(5001,()=>{
+appp.listen(5001,()=>{
     console.log("server is running at port 5001")
 })
